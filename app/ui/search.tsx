5583a9ad19b7b835bed17062f1ebafca2360f632
qyy <1069542931@qq.com>
2024-07-13 21:56:25 +0800

    perfect search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 export default function Search({ placeholder }: { placeholder: string }) {
 
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const {replace} = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const param = new URLSearchParams(searchParams)
+    if(term) {
+      param.set('query', term);
+    }
+    else {
+      param.delete('query');
+    }
+    replace(`${pathname}?${param.toString()}`);
   }
 
   return (
